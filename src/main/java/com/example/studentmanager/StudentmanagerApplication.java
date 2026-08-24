d4aa2be637435f66d38726695252645a114a88dc
thanh104511-create <thanh104511@donga.edu.vn>
2026-08-24 11:21:14 +0700

    Add files via upload

---
+++ src/main/java/com/example/studentmanager/StudentmanagerApplication.java
@@ -0,0 +1,23 @@
+package com.example.studentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/api")
+public class StudentmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentmanagerApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot API";
+    }
+
+}
